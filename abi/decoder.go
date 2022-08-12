a269ea12414c6bf7d2ebdde26d1218ca4f4d1f9d
a <a@a.a>
2022-08-11 20:49:47 -0500

    test pass

---
+++ abi/decoder.go
@@ -0,0 +1,186 @@
+package abi
+
+import (
+	"errors"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var (
+	// MaxUint256 is the maximum value that can be represented by a uint256.
+	MaxUint256 = new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 256), common.Big1)
+	// MaxInt256 is the maximum value that can be represented by a int256.
+	MaxInt256 = new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 255), common.Big1)
+)
+
+type Decoder struct {
+	xs []byte
+
+	cur           int
+	dynamicLayers []int
+}
+
+func NewDecoder(xs []byte) *Decoder {
+	return &Decoder{
+		xs: xs,
+	}
+}
+
+func (d *Decoder) ReadWord() (o [32]byte, err error) {
+	_, err = d.Read(o[:])
+	if err != nil {
+		return
+	}
+	return o, nil
+}
+
+func (d *Decoder) ReadN(n int) ([]byte, error) {
+	o := make([]byte, n)
+	_, err := d.Read(o[:])
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (d *Decoder) ReadNPadRight32(n int) ([]byte, error) {
+	diff := 32 - (n % 32)
+	if diff == 32 {
+		diff = 0
+	}
+	o := make([]byte, n)
+	_, err := d.Read(o[:])
+	if err != nil {
+		return nil, err
+	}
+	_, err = d.ReadN(diff)
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+func (d *Decoder) ReadNPadLeft32(n int) ([]byte, error) {
+	diff := 32 - (n % 32)
+	if diff == 32 {
+		diff = 0
+	}
+	_, err := d.ReadN(diff)
+	if err != nil {
+		return nil, err
+	}
+	o := make([]byte, n)
+	_, err = d.Read(o[:])
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (d *Decoder) Read(o []byte) (int, error) {
+	if (len(d.xs) - d.cur) < len(o) {
+		return 0, errors.New("abi: unexpected EOF")
+	}
+	n := copy(o, d.xs[d.cur:d.cur+len(o)])
+	d.cur = d.cur + len(o)
+	return n, nil
+}
+
+func (d *Decoder) ReadBigUint() (big.Int, error) {
+	b, err := d.ReadWord()
+	if err != nil {
+		return big.Int{}, err
+	}
+	ret := new(big.Int).SetBytes(b[:])
+	return *ret, nil
+}
+
+func (d *Decoder) ReadAddress() (common.Address, error) {
+	word, err := d.ReadWord()
+	if err != nil {
+		return common.Address{}, err
+	}
+	ans := common.BytesToAddress(word[:])
+	return ans, nil
+}
+
+func (d *Decoder) ReadBigInt() (big.Int, error) {
+	rt, err := d.ReadBigUint()
+	if err != nil {
+		return big.Int{}, err
+	}
+	ret := &rt
+	if ret.Bit(255) == 1 {
+		ret.Add(MaxUint256, new(big.Int).Neg(ret))
+		ret.Add(ret, common.Big1)
+		ret.Neg(ret)
+	}
+	return *ret, nil
+}
+
+func (d *Decoder) ReadBool() (bool, error) {
+	ans, err := d.ReadBigUint()
+	if err != nil {
+		return false, err
+	}
+	if ans.Cmp(big.NewInt(0)) == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (d *Decoder) ReadInt() (int, error) {
+	ans, err := d.ReadBigInt()
+	if err != nil {
+		return 0, err
+	}
+	if !ans.IsInt64() {
+		return 0, errors.New("abi: int overflow")
+	}
+	return int(ans.Int64()), nil
+}
+
+func (d *Decoder) ReadUint() (uint, error) {
+	ans, err := d.ReadBigUint()
+	if err != nil {
+		return 0, err
+	}
+	if !ans.IsUint64() {
+		return 0, errors.New("abi: int overflow")
+	}
+	return uint(ans.Uint64()), nil
+}
+
+func (d *Decoder) ReadUint8() (uint8, error) {
+	ans, err := d.ReadInt()
+	if err != nil {
+		return 0, err
+	}
+	if ans > 255 {
+		return 0, errors.New("abi: uint8 overflow")
+	}
+	return uint8(ans), nil
+}
+
+func (d *Decoder) ReadUint16() (uint16, error) {
+	ans, err := d.ReadInt()
+	if err != nil {
+		return 0, err
+	}
+	if ans > 65536 {
+		return 0, errors.New("abi: uint16 overflow")
+	}
+	return uint16(ans), nil
+}
+
+func (d *Decoder) ReadDynamic() (*Decoder, error) {
+	offset, err := d.ReadBigUint()
+	if err != nil {
+		return nil, err
+	}
+	actual := int(offset.Uint64())
+	if len(d.xs) < actual {
+		return nil, errors.New("abi: dynamic overflow")
+	}
+	return NewDecoder(d.xs[actual:]), nil
+}
---
+++ abi/decoder_test.go
@@ -0,0 +1,125 @@
+package abi
+
+import (
+	"encoding/hex"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestBasicExample(t *testing.T) {
+	dec := hexDecode(`
+0000000000000000000000000000000000000000000000000000000000000045
+0000000000000000000000000000000000000000000000000000000000000001
+`)
+	type baz struct {
+		x int
+		y bool
+	}
+
+	var r baz
+	var err error
+
+	r.x, err = dec.ReadInt()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r.y, err = dec.ReadBool()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.x != 69 {
+		t.Errorf("expect %d got %d", 69, r.x)
+	}
+	if r.y != true {
+		t.Errorf("expect %v got %v", true, r.y)
+	}
+}
+
+func TestDynamicExample(t *testing.T) {
+
+	dec := hexDecode(`
+0000000000000000000000000000000000000000000000000000000000000123
+0000000000000000000000000000000000000000000000000000000000000080
+3132333435363738393000000000000000000000000000000000000000000000
+00000000000000000000000000000000000000000000000000000000000000e0
+0000000000000000000000000000000000000000000000000000000000000002
+0000000000000000000000000000000000000000000000000000000000000456
+0000000000000000000000000000000000000000000000000000000000000789
+000000000000000000000000000000000000000000000000000000000000000d
+48656c6c6f2c20776f726c642100000000000000000000000000000000000000
+	`)
+
+	type f struct {
+		a int
+		b []uint32
+		c []byte
+		d string
+	}
+
+	var r f
+	var err error
+	r.a, err = dec.ReadInt()
+	if err != nil {
+		t.Fatal("r.a", err)
+	}
+
+	arr_b, err := dec.ReadDynamic()
+	if err != nil {
+		t.Fatal("r.b", err)
+	}
+
+	array_len, err := arr_b.ReadInt()
+	if err != nil {
+		t.Fatal("r.b_len", err)
+	}
+	r.b = make([]uint32, 0, array_len)
+	for i := 0; i < array_len; i++ {
+		val, err := arr_b.ReadUint()
+		if err != nil {
+			t.Fatal("r.b_inside", err)
+			t.Fatal(err)
+		}
+		r.b = append(r.b, uint32(val))
+	}
+
+	r.c, err = dec.ReadNPadRight32(10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	arr_d, err := dec.ReadDynamic()
+	if err != nil {
+		t.Fatal("arr_d", err)
+	}
+	str_len, err := arr_d.ReadInt()
+	if err != nil {
+		t.Fatal("str_len", err)
+	}
+	bts, err := arr_d.ReadNPadRight32(str_len)
+	if err != nil {
+		t.Fatal("str", err)
+	}
+	r.d = string(bts)
+	if !reflect.DeepEqual(r.a, 0x123) {
+		t.Errorf("expect %v got %v", 0x123, r.a)
+	}
+	if !reflect.DeepEqual(r.b, []uint32{0x456, 0x789}) {
+		t.Errorf("expect %v got %v", []uint32{0x456, 0x789}, r.b)
+	}
+	if !(string(r.c) == "1234567890") {
+		t.Errorf("expect %v got %v", true, r.c)
+	}
+}
+
+func hexDecode(s string) *Decoder {
+	s = strings.TrimPrefix(s, "0x")
+	s = strings.ReplaceAll(s, "\n", "")
+	s = strings.ReplaceAll(s, "\r", "")
+	s = strings.ReplaceAll(s, " ", "")
+	s = strings.ReplaceAll(s, `	`, "")
+	ans, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return NewDecoder(ans)
+}
