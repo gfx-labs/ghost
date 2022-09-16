635d0f56ec79fdedaf9fe7082f946bfbbea1c0b3
a <a@a.a>
2022-09-16 03:35:43 -0500

    remove errors

--- abi/encoder.go
+++ abi/encoder.go
@@ -18,77 +18,73 @@ func NewEncoder(xs []byte) *Encoder {
 	}
 }
 
-func (d *Encoder) Write(o []byte) (int, error) {
+func (d *Encoder) Write(o []byte) int {
 	d.xs = append(d.xs, o...)
-	return len(o), nil
+	return len(o)
 }
 
-func (d *Encoder) WriteNPadRight32(xs []byte) error {
+func (d *Encoder) WriteNPadRight32(xs []byte) {
 	diff := 32 - (len(xs) % 32)
 	if diff == 32 {
 		diff = 0
 	}
 	o := append(xs, make([]byte, diff)...)
-	_, err := d.Write(o[:])
-	if err != nil {
-		return err
-	}
-	return nil
+	_ = d.Write(o[:])
+	return
 }
-func (d *Encoder) WriteWord(xs []byte) error {
+func (d *Encoder) WriteWord(xs []byte) {
 	diff := 32 - (len(xs) % 32)
 	if diff == 32 {
 		diff = 0
 	}
 	o := append(make([]byte, diff), xs...)
-	_, err := d.Write(o[:])
-	if err != nil {
-		return err
-	}
-	return nil
+	_ = d.Write(o[:])
+	return
 }
 
-func (d *Encoder) WriteBigUint(a *uint256.Int) error {
-	return d.WriteWord(a.Bytes())
+func (d *Encoder) WriteBigUint(a *uint256.Int) {
+	d.WriteWord(a.Bytes())
 }
 
-func (d *Encoder) WriteAddress(a common.Address) error {
-	return d.WriteWord(a[:])
+func (d *Encoder) WriteAddress(a common.Address) {
+	d.WriteWord(a[:])
 }
 
-func (d *Encoder) WriteBigInt(ret *big.Int) error {
+func (d *Encoder) WriteBigInt(ret *big.Int) {
 	nr := new(big.Int).Set(ret)
 	if ret.Cmp(new(big.Int)) == -1 {
 		nr.Neg(nr)
 		nr.Sub(nr, common.Big1)
 		nr.Sub(nr, MaxUint256)
 		nr.Neg(nr)
-		return d.WriteWord(nr.Bytes())
+		d.WriteWord(nr.Bytes())
+		return
 	}
-	return d.WriteWord(ret.Bytes())
+	d.WriteWord(ret.Bytes())
 }
 
-func (d *Encoder) WriteBool(b bool) error {
+func (d *Encoder) WriteBool(b bool) {
 	if b == true {
-		return d.WriteWord([]byte{0x1})
+		d.WriteWord([]byte{0x1})
+		return
 	}
-	return d.WriteWord([]byte{0x0})
+	d.WriteWord([]byte{0x0})
 }
 
-func (d *Encoder) WriteInt(i int) error {
-	return d.WriteBigInt(big.NewInt(int64(i)))
+func (d *Encoder) WriteInt(i int) {
+	d.WriteBigInt(big.NewInt(int64(i)))
 }
 
-func (d *Encoder) WriteUint(i uint) error {
-	return d.WriteBigUint(uint256.NewInt(uint64(i)))
+func (d *Encoder) WriteUint(i uint) {
+	d.WriteBigUint(uint256.NewInt(uint64(i)))
 }
 
-func (d *Encoder) WriteUint8(i uint8) error {
-	return d.WriteUint(uint(i))
+func (d *Encoder) WriteUint8(i uint8) {
+	d.WriteUint(uint(i))
 }
 
-func (d *Encoder) WriteUint16(i uint16) error {
-	return d.WriteUint(uint(i))
+func (d *Encoder) WriteUint16(i uint16) {
+	d.WriteUint(uint(i))
 }
 
 func (d *Encoder) Finish() []byte {
@@ -98,10 +94,10 @@ func (d *Encoder) Reset() {
 	d.xs = d.xs[:0]
 }
 
-func (d *Encoder) WriteDynamic(*Encoder) error {
+func (d *Encoder) WriteDynamic(*Encoder) {
 	panic("not done")
 }
 
-func (d *Encoder) WriteString() (string, error) {
+func (d *Encoder) WriteString(string) {
 	panic("not done")
 }
