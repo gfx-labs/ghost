abe0a2b45efad5dae085b3541c088250edb3a41d
a <a@a.a>
2022-09-16 03:40:23 -0500

    chainable builder

--- abi/encoder.go
+++ abi/encoder.go
@@ -18,17 +18,19 @@ func NewBuilder(xs []byte) *Builder {
 	}
 }
 
-func (d *Builder) Write(o []byte) {
+func (d *Builder) Write(o []byte) *Builder {
 	d.xs = append(d.xs, o...)
+	return d
 }
 
-func (d *Builder) WriteNPadRight32(xs []byte) {
+func (d *Builder) WriteNPadRight32(xs []byte) *Builder {
 	diff := 32 - (len(xs) % 32)
 	if diff == 32 {
 		diff = 0
 	}
 	o := append(xs, make([]byte, diff)...)
 	d.Write(o[:])
+	return d
 }
 func (d *Builder) WriteWord(xs []byte) {
 	diff := 32 - (len(xs) % 32)
@@ -40,15 +42,17 @@ func (d *Builder) WriteWord(xs []byte) {
 	return
 }
 
-func (d *Builder) WriteBigUint(a *uint256.Int) {
+func (d *Builder) WriteBigUint(a *uint256.Int) *Builder {
 	d.WriteWord(a.Bytes())
+	return d
 }
 
-func (d *Builder) WriteAddress(a common.Address) {
+func (d *Builder) WriteAddress(a common.Address) *Builder {
 	d.WriteWord(a[:])
+	return d
 }
 
-func (d *Builder) WriteBigInt(ret *big.Int) {
+func (d *Builder) WriteBigInt(ret *big.Int) *Builder {
 	nr := new(big.Int).Set(ret)
 	if ret.Cmp(new(big.Int)) == -1 {
 		nr.Neg(nr)
@@ -56,33 +60,39 @@ func (d *Builder) WriteBigInt(ret *big.Int) {
 		nr.Sub(nr, MaxUint256)
 		nr.Neg(nr)
 		d.WriteWord(nr.Bytes())
-		return
+		return d
 	}
 	d.WriteWord(ret.Bytes())
+	return d
 }
 
-func (d *Builder) WriteBool(b bool) {
+func (d *Builder) WriteBool(b bool) *Builder {
 	if b == true {
 		d.WriteWord([]byte{0x1})
-		return
+		return d
 	}
 	d.WriteWord([]byte{0x0})
+	return d
 }
 
-func (d *Builder) WriteInt(i int) {
+func (d *Builder) WriteInt(i int) *Builder {
 	d.WriteBigInt(big.NewInt(int64(i)))
+	return d
 }
 
-func (d *Builder) WriteUint(i uint) {
+func (d *Builder) WriteUint(i uint) *Builder {
 	d.WriteBigUint(uint256.NewInt(uint64(i)))
+	return d
 }
 
-func (d *Builder) WriteUint8(i uint8) {
+func (d *Builder) WriteUint8(i uint8) *Builder {
 	d.WriteUint(uint(i))
+	return d
 }
 
-func (d *Builder) WriteUint16(i uint16) {
+func (d *Builder) WriteUint16(i uint16) *Builder {
 	d.WriteUint(uint(i))
+	return d
 }
 
 func (d *Builder) Finish() []byte {
@@ -92,10 +102,13 @@ func (d *Builder) Reset() {
 	d.xs = d.xs[:0]
 }
 
-func (d *Builder) WriteDynamic(*Builder) {
+func (d *Builder) StartDynamic(*Builder) *Builder {
+	panic("not done")
+}
+func (d *Builder) FinishDynamic(*Builder) *Builder {
 	panic("not done")
 }
 
-func (d *Builder) WriteString(string) {
+func (d *Builder) WriteString(string) *Builder {
 	panic("not done")
 }
