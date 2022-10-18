4a3008df7ea0e58b487ec41ab2c21078bcf47fc5
a <a@a.a>
2022-10-17 21:01:22 -0500

    new abi encoder

--- abi/encoder.go
+++ abi/encoder.go
@@ -1,56 +1,135 @@
 package abi
 
 import (
+	"bytes"
+	"encoding/binary"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/holiman/uint256"
 )
 
+// the word, which contains either pointer or bytes data
+type word struct {
+	pointer int
+	dat     []byte
+}
+
+func padRight32(xs []byte) []byte {
+	pl := 32 * (len(xs) / 32)
+	if pl == 0 {
+		pl = 32
+	}
+	return rightPadBytes(xs, pl)
+}
+func padLeft32(xs []byte) []byte {
+	pl := 32 * (len(xs) / 32)
+	if pl == 0 {
+		pl = 32
+	}
+	r := leftPadBytes(xs, pl)
+	return r
+}
+
+func rightPadBytes(slice []byte, l int) []byte {
+	if l <= len(slice) {
+		return slice
+	}
+	padded := make([]byte, l-len(slice))
+	return append(slice, padded...)
+}
+
+func leftPadBytes(slice []byte, l int) []byte {
+	if l <= len(slice) {
+		return slice
+	}
+	padded := make([]byte, l-len(slice))
+	return append(padded, slice...)
+}
+
 type Builder struct {
 	parent *Builder
 
-	cur  int
-	args [][2][]byte
+	m memory
+	w *bytes.Buffer
+}
 
-	dsz int
+// either returns dat if pointer is 0, or the 32 bytes of word
+func (v *word) StackBytes() []byte {
+	if v.pointer == 0 {
+		return v.dat
+	}
+	o := [8]byte{}
+	binary.BigEndian.PutUint64(o[:], uint64(v.pointer))
+	return padLeft32(o[:])
+}
 
-	prefix []byte
+type memory struct {
+	stack []word
+
+	// current stack pointer
+	cur int
+
+	heap [][]byte
+	// current heap size
+	hz int
+}
+
+func (m *memory) WriteStack(xs []byte) {
+	xs = padRight32(xs)
+	m.stack = append(m.stack, word{
+		pointer: 0,
+		dat:     xs,
+	})
+	m.cur = m.cur + len(xs)
+	// increment existing pointers, which are now wrong since the initial offset has increased
+	for k := range m.stack {
+		if m.stack[k].pointer != 0 {
+			m.stack[k].pointer = m.stack[k].pointer + len(xs)
+		}
+	}
+}
+
+func (m *memory) WriteDynamic(xs []byte) {
+	xs = padRight32(xs)
+	// add a chunk to the stack cursor
+	m.cur = m.cur + 32
+	// the current pointer is at the heap + current stack. add that to the stack
+	m.stack = append(m.stack, word{
+		pointer: m.hz + m.cur,
+	})
+	// now advance the heap cursor
+	m.hz = m.hz + len(xs)
+	// and append to heap
+	m.heap = append(m.heap, xs)
 }
 
 func NewBuilder(xs []byte) *Builder {
-	return &Builder{prefix: xs}
+	return NewBuilderRaw(bytes.NewBuffer(xs))
+}
+
+func NewBuilderRaw(w *bytes.Buffer) *Builder {
+	return &Builder{
+		w: w,
+	}
 }
 
 func (d *Builder) WriteNPadRight32(xs []byte) *Builder {
-	diff := 32 - (len(xs) % 32)
-	if diff == 32 {
-		diff = 0
-	}
-	o := append(xs, make([]byte, diff)...)
-	d.args = append(d.args, [2][]byte{o, nil})
+	d.m.WriteStack(padRight32(xs))
 	return d
 }
 func (d *Builder) WriteWord(xs []byte) {
-	diff := 32 - (len(xs) % 32)
-	if diff == 32 {
-		diff = 0
-	}
-	o := append(make([]byte, diff), xs...)
-	d.args = append(d.args, [2][]byte{o, nil})
+	d.m.WriteStack(padLeft32(xs))
 	return
 }
-
 func (d *Builder) WriteBigUint(a *uint256.Int) *Builder {
 	d.WriteWord(a.Bytes())
 	return d
 }
-
 func (d *Builder) WriteAddress(a common.Address) *Builder {
 	d.WriteWord(a[:])
 	return d
 }
-
 func (d *Builder) WriteBigInt(ret *big.Int) *Builder {
 	nr := new(big.Int).Set(ret)
 	if ret.Cmp(new(big.Int)) == -1 {
@@ -64,7 +143,6 @@ func (d *Builder) WriteBigInt(ret *big.Int) *Builder {
 	d.WriteWord(ret.Bytes())
 	return d
 }
-
 func (d *Builder) WriteBool(b bool) *Builder {
 	if b == true {
 		d.WriteWord([]byte{0x1})
@@ -73,66 +151,64 @@ func (d *Builder) WriteBool(b bool) *Builder {
 	d.WriteWord([]byte{0x0})
 	return d
 }
-
 func (d *Builder) WriteInt(i int) *Builder {
 	d.WriteBigInt(big.NewInt(int64(i)))
 	return d
 }
-
 func (d *Builder) WriteUint(i uint) *Builder {
 	d.WriteBigUint(uint256.NewInt(uint64(i)))
 	return d
 }
-
 func (d *Builder) WriteUint8(i uint8) *Builder {
 	d.WriteUint(uint(i))
 	return d
 }
-
 func (d *Builder) WriteUint16(i uint16) *Builder {
 	d.WriteUint(uint(i))
 	return d
 }
 
-func (d *Builder) Finish() []byte {
-	out := make([]byte, 0, 32*len(d.args)+d.dsz)
-	out = append(out, d.prefix...)
-	free := len(d.args) * 32
-	for _, v := range d.args {
-		if len(v[0]) == 0 {
-			b32 := uint256.NewInt(uint64(free)).Bytes32()
-			out = append(out, b32[:]...)
-		} else {
-			out = append(out, v[0]...)
+func (d *Builder) Close() error {
+	for _, v := range d.m.stack {
+		_, err := d.w.Write(v.StackBytes())
+		if err != nil {
+			return err
 		}
-		free = free + len(v[1])
 	}
-	for _, v := range d.args {
-		out = append(out, v[1]...)
+	for _, v := range d.m.heap {
+		_, err := d.w.Write(v)
+		if err != nil {
+			return err
+		}
 	}
-	d.args = d.args[:0]
-	return out
+	return nil
+}
+
+func (d *Builder) Finish() []byte {
+	d.Close()
+	return d.w.Bytes()
 }
 
 func (d *Builder) Reset() {
-	d.args = d.args[:0]
+	d.w.Reset()
 }
 
 func (d *Builder) EnterDynamic(l int) *Builder {
-	b := &Builder{parent: d}
-	if l > 0 {
-		b32 := uint256.NewInt(uint64(l)).Bytes32()
-		b.prefix = b32[:]
+	b := &Builder{
+		parent: d,
 	}
+	b.WriteInt(l)
 	return b
 }
 func (d *Builder) ExitDynamic() *Builder {
 	if d.parent == nil {
 		panic("tried to exit dynamic when not in one")
 	}
-	bts := d.Finish()
-	d.parent.args = append(d.parent.args, [2][]byte{nil, bts})
-	d.dsz = d.dsz + len(bts)
+	//TODO: a buf pool will reduce allocations here
+	buf := new(bytes.Buffer)
+	d.w = buf
+	chile := d.Finish()
+	d.parent.m.WriteDynamic(chile)
 	return d.parent
 }
 
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	gfx.cafe/util/go/generic v0.0.0-20221003014715-6000e93338e0
 	github.com/ethereum/go-ethereum v1.10.25
 	github.com/holiman/uint256 v1.2.1
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a
 )
 
@@ -16,27 +17,28 @@ require (
 	git.tuxpa.in/a/zlog v1.32.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.5.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
-	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0 // indirect
 	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
 	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
