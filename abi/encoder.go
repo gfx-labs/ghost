d9b34863c67ccf9fde996055a510cf7ad4bc921d
a <a@tuxpa.in>
2022-11-23 01:56:50 -0600

    memoery

--- abi/encoder.go
+++ abi/encoder.go
@@ -38,10 +38,16 @@ func (m *memory) Insert(loc int, data []byte) {
 	if loc == -1 {
 		loc = m.cur
 	}
-	if len(m.encoded) == m.cur {
+	var s []byte
+	if loc == 0 {
+		s = data
+	} else {
+		s = append(m.encoded[:loc], data...)
+	}
+	if loc == len(m.encoded) {
 		m.encoded = s
 	} else {
-		m.encoded = append(s, m.encoded[m.cur:]...)
+		m.encoded = append(s, m.encoded[loc:]...)
 	}
 	m.Pos(len(data))
 }
